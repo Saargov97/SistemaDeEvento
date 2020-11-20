fee62d502f534ed4553aa6d1fde6db74779e17e7
Rafael <saargov97@gmail.com>
2020-11-20 14:44:39 -0300

    Naming

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <v-toolbar fixed dark class="cyan">
     <v-toolbar-title class="mr-4">
-      <span class="home" @click="navigateTo({name: 'root'})">FinControl</span>
+      <span class="home" @click="navigateTo({name: 'root'})">Event</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-btn v-if="$store.state.isUserLoggedIn" @click="navigateTo({name: 'banks'})">Bank</v-btn>
