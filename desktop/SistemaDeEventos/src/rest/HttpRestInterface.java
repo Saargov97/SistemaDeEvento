34ecbc1e25824bb48bb616f732058902136a5980
Rafael <saargov97@gmail.com>
2020-11-24 22:17:20 -0300

    Correct IP server

--- desktop/SistemaDeEventos/src/rest/HttpRestInterface.java
+++ desktop/SistemaDeEventos/src/rest/HttpRestInterface.java
@@ -4,7 +4,7 @@ public interface HttpRestInterface {
     final HttpRestPrincipal httpRestPrincipal = new HttpRestPrincipal();
 
     final String USER_AGENT = "Mozilla/5.0";
-    static final String urlDefault = "http://177.44.248.1:8081";
+    static final String urlDefault = "http://177.44.248.81:8081";
 
     public int getResponseCode();
 }
