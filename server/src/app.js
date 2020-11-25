a1a75379d662fd2399ed6d8316fc8e9f0e40901a
Rafael <saargov97@gmail.com>
2020-11-24 22:02:28 -0300

    Reset db and webclient in server

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 require('./routes')(app)
 
-sequelize.sync({force: false})
+sequelize.sync({force: true})
   .then(() => {
     app.listen(config.port)
     console.log(`Up and running, on port: ${config.port}`)
